2a14cf3842f26a6eb6888cdb22c661346c525300
luzialeticia <leticialuzia@tutamail.com>
2020-09-21 23:53:18 -0300

    Criando as variáveis que chamam o os getElementById e aplicando as funções. Criando uma função calcular e colocando as outras funções dentro dela

--- js/script.js
+++ js/script.js
@@ -1,61 +1,66 @@
-const sum = (a, b) => a + b
+const calcular = () => {
+    const sum = (a, b) => a + b
 
-const subtractionA = (a, b) => a - b
-const subtractionB = (a, b) => b - a
-
-const multiplication = (a, b) => a * b
-
-const divisionA = (a, b) => {
-    if (b === 0) {
-        return `Não é possível efetuar divisão por 0`
-    } else {
-        return a / b
-    }
-}
-const divisionB = (a, b) => {
-    if (a === 0) {
-        return `Não é possível efetuar divisão por 0`
-    } else {
-        return b / a
-    }
-}
-
-const exponent = (num) => Math.pow(num, 2)
-
-const divider = (num) => {
-   const dividers = []
-   let howMuchDividers;
-   let dividersToString;
-   let dividersToPlural = `divisor`
-
-   for (let i = 1; i <= num; i++) {
-       if (num % i === 0) {
-           dividers.push(i)
-       }
-   }
-
-   howMuchDividers = dividers.length
-   dividersToString = dividers.join(', ')
-
-   if (howMuchDividers > 1) {
-       dividersToPlural = `divisores`
-   }
-
-   return `${dividersToString} (${howMuchDividers} ${dividersToPlural})`
-}
-
-const factorial = (num) => {
-    if (num <= 21) {
-        let factorial = 1
-
-        for (let i = 1; i <= num; i++) {
-            factorial = factorial * i
+    const subtractionA = (a, b) => a - b
+    const subtractionB = (a, b) => b - a
+    
+    const multiplication = (a, b) => a * b
+    
+    const divisionA = (a, b) => {
+        if (b === 0) {
+            return `Não é possível efetuar divisão por 0`
+        } else {
+            return a / b
         }
-        return factorial
-    } else {
-        return `Número muito grande.`
     }
+    const divisionB = (a, b) => {
+        if (a === 0) {
+            return `Não é possível efetuar divisão por 0`
+        } else {
+            return b / a
+        }
+    }
+    
+    const exponent = (num) => Math.pow(num, 2)
+    
+    const divider = (num) => {
+       const dividers = []
+       let howMuchDividers;
+       let dividersToString;
+       let dividersToPlural = `divisor`
+    
+       for (let i = 1; i <= num; i++) {
+           if (num % i === 0) {
+               dividers.push(i)
+           }
+       }
+    
+       howMuchDividers = dividers.length
+       dividersToString = dividers.join(', ')
+    
+       if (howMuchDividers > 1) {
+           dividersToPlural = `divisores`
+       }
+    
+       return `${dividersToString} (${howMuchDividers} ${dividersToPlural})`
+    }
+    
+    const factorial = (num) => {
+        if (num <= 21) {
+            let factorial = 1
+    
+            for (let i = 1; i <= num; i++) {
+                factorial = factorial * i
+            }
+            return factorial
+        } else {
+            return `Número muito grande.`
+        }
+    }
+    
+    const numberA = document.getElementById('numberA').valueAsNumber
+    const numberB = document.getElementById('numberB').valueAsNumber
+    let resultSum = document.getElementById('sum').valueAsNumber
+    
+    resultSum = sum(numberA, numberB)
 }
-
-const numberA = document.getElementById('numberA')
-const numberB = document.getElementById('numberB')
